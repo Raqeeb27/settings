83bec67b3410ced47391b033dc013a5347d0532a
Mohammed Abdul Raqeeb <raqeeb2709@gmail.com>
2024-05-10 16:03:32 +0530

    Added methods silent and volme up

--- settings.py
+++ settings.py
@@ -5,13 +5,16 @@ from re import match
 from colorama import Fore as f, Style as s
 
 
+## ===========================================================================
+### Variables
+
 start_time = localtime()
 start_hour = int(strftime("%H", start_time))
 start_min = int(strftime("%M", start_time))
 start_sec = int(strftime("%S", start_time))
 
 torch_state = False
-notificationList = ['Say \'SubhanAllah\' X 3',
+NOTIFICATION_LIST = ['Say \'SubhanAllah\' X 3',
                     'Say \'Allhamdulillah\' X 3',
                     'Say \'AllahuAkbar\' X 4',
                     'Say \'Laa Ilaaha Illah....\'',
@@ -23,34 +26,40 @@ notificationList = ['Say \'SubhanAllah\' X 3',
                     'Say \'Astagfirullah\' X 3',
                     '\'Allahu Akbar\' while Climbing \'UP\'',
                     '\'SubhanAllah\' while Climbing \'DOWN\'']
-allowedPhoneNumberChar = r'^[0-9*+#]+$'
+ALLOWED_PHONE_CHARS = r'^[0-9*+#]+$'
 
-cyan = f.CYAN
-light_cyan = f.LIGHTCYAN_EX
-yellow = f.YELLOW
-red = f.RED
-bold = s.BRIGHT
-dim, reset = s.DIM, s.RESET_ALL
+CYAN = f.CYAN
+LIGHT_CYAN = f.LIGHTCYAN_EX
+YELLOW = f.YELLOW
+RED = f.RED
+BOLD = s.BRIGHT
+DIM, RESET = s.DIM, s.RESET_ALL
 
 
+## ===========================================================================
+### Functions
+
+# Function to toggle torch
 def termx_torch():
     global torch_state
 
-    print(f"{yellow}{bold}\n------ TORCH ------\n{reset}")
+    print(f"{YELLOW}{BOLD}\n------ TORCH ------\n{RESET}")
 
     if torch_state :
         os.system("termux-torch off")
-        print(f"{bold}Torch is set \'OFF\'\n{reset}")
+        print(f"{BOLD}Torch is set \'OFF\'\n{RESET}")
         torch_state = False
 
     else:
         os.system("termux-torch on")
-        print(f"{bold}Torch is set \'ON\'\n{reset}")
+        print(f"{BOLD}Torch is set \'ON\'\n{RESET}")
         torch_state = True
 
+## --------------------------------------------------------------------------
+# Function to a call
 def termux_telephony_call():
     while True:
-        print(f"{yellow}{bold}\n------ CALL ------\n{reset}")
+        print(f"{YELLOW}{BOLD}\n------ CALL ------\n{RESET}")
 
         phoneNumber = input("Enter Phone Number : ")
 
@@ -63,9 +72,9 @@ def termux_telephony_call():
             print("Call Cancelled.\n")
             break
 
-        if not (len(phoneNumber) in [4,5,phoneNumberLength] and phoneNumber.count('+') in [0,1] and match(allowedPhoneNumberChar, phoneNumber)):
-            print(f"{red}\nInvalid Phone Number. Call not sent.\n{dim}"
-                    f"Enter number as '0' if you want to Cancel{reset}")
+        if not (len(phoneNumber) in [4,5,phoneNumberLength] and phoneNumber.count('+') in [0,1] and match(ALLOWED_PHONE_CHARS, phoneNumber)):
+            print(f"{RED}\nInvalid Phone Number. Call not sent.\n{DIM}"
+                    f"Enter number as '0' if you want to Cancel{RESET}")
             continue
 
         os.system(f"termux-telephony-call {phoneNumber}")
@@ -78,10 +87,12 @@ def termux_telephony_call():
         print("\n")
         exit()
 
+## --------------------------------------------------------------------------
+# Function to send sms
 def termux_sms_send():
     while True:
         try:
-            print(f"{yellow}{bold}\n------ Send SMS ------\n{reset}")
+            print(f"{YELLOW}{BOLD}\n------ Send SMS ------\n{RESET}")
 
             phoneNumber = int(input("Enter Phone Number : "))
 
@@ -90,14 +101,14 @@ def termux_sms_send():
                 break
 
             if not (len(str(phoneNumber)) in [10]):
-                print(f"{red}\nInvalid Phone Number. SMS not sent.\n{dim}"
-                    f"Enter number as '0' if you want to Cancel{reset}")
+                print(f"{RED}\nInvalid Phone Number. SMS not sent.\n{DIM}"
+                    f"Enter number as '0' if you want to Cancel{RESET}")
                 continue
 
             text = input("\nEnter message : ")
 
             if text.count(" ") == len(text) or len(text) == 0:
-                print(f"{red}\nEmpty message can't be sent\n{reset}")
+                print(f"{RED}\nEmpty message can't be sent\n{RESET}")
                 break
 
             os.system(f"termux-sms-send -n {phoneNumber} {text}")
@@ -112,11 +123,13 @@ def termux_sms_send():
             break
 
         except ValueError:
-            print(f"{red}\nInvalid Phone Number. SMS not sent.\n{dim}"
-                f"Enter phno. as '0' if you want to Cancel{reset}")
+            print(f"{RED}\nInvalid Phone Number. SMS not sent.\n{DIM}"
+                f"Enter phno. as '0' if you want to Cancel{RESET}")
 
+## --------------------------------------------------------------------------
+# Function to toggle wifi
 def termux_wifi_enable():
-    print(f"{yellow}{bold}\n------ WIFI ------{reset}\n")
+    print(f"{YELLOW}{BOLD}\n------ WIFI ------{RESET}\n")
 
     wifi_state = input("Enter ON (o) or OFF (f) : ").upper()
 
@@ -129,10 +142,12 @@ def termux_wifi_enable():
         print("WIFI is set\'OFF\'\n")
 
     else :
-        print(f"{red}{dim}Please enter between \'o\' or \'f\'\n{reset}")
+        print(f"{RED}{DIM}Please enter between \'o\' or \'f\'\n{RESET}")
 
+## --------------------------------------------------------------------------
+# Function to get notifications
 def termux_notification():
-    global notificationList, start_hour, start_min
+    global NOTIFICATION_LIST, start_hour, start_min
 
     notificationSound = input("\nPlay Notification sound.... "
                                 f"Phone will be removed from Silent Mode [\'y\' or \'n\'] : ").upper()
@@ -150,14 +165,14 @@ def termux_notification():
     try:
         notificationDelay = int(input("\nEnter Time Delay for next Notification (in Minutes <= 60) : "))
         if notificationDelay > 60 :
-            print(f"{red}{dim}Enter Delay less than hour (60 min)\n{reset}")
+            print(f"{RED}{DIM}Enter Delay less than hour (60 min)\n{RESET}")
             return
         elif notificationDelay <= 0:
-            print(f"{red}{dim}\nMinutes should be a Positive Integer Value < \'60\'\n{reset}")
+            print(f"{RED}{DIM}\nMinutes should be a Positive Integer Value < \'60\'\n{RESET}")
             return
 
     except ValueError :
-        print(f"{red}{dim}\nMinutes should be a Positive Integer Value < \'60\'\n{reset}")
+        print(f"{RED}{DIM}\nMinutes should be a Positive Integer Value < \'60\'\n{RESET}")
         return
 
     currentLocalTime = localtime()
@@ -167,7 +182,7 @@ def termux_notification():
     if start_min != current_min:
         start_min = current_min
 
-    print(f"{yellow}{bold}\n------ Getting Random Notification ------{reset}")
+    print(f"{YELLOW}{BOLD}\n------ Getting Random Notification ------{RESET}")
 
     if start_min + notificationDelay > 59:
         current_hour = current_hour + 1
@@ -176,16 +191,16 @@ def termux_notification():
         display_min = start_min + notificationDelay
 
     if display_min < 10:
-        print(f"{dim}Next Notification at : --- {current_hour}:0{display_min} ---{reset}")
+        print(f"{DIM}Next Notification at : --- {current_hour}:0{display_min} ---{RESET}")
     else:
-        print(f"{dim}Next Notification at : --- {current_hour}:{display_min} ---{reset}")
+        print(f"{DIM}Next Notification at : --- {current_hour}:{display_min} ---{RESET}")
 
-    print(f"{dim}Press \' ctrl + c \' to Cancel...{reset}\n")
+    print(f"{DIM}Press \' ctrl + c \' to Cancel...{RESET}\n")
 
     while True:
         try:
             next_notification_min = start_min + notificationDelay
-            randamNotification = choice(notificationList)
+            randamNotification = choice(NOTIFICATION_LIST)
 
             currentLocalTime = localtime()
             current_hour = int(strftime("%H", currentLocalTime))
@@ -211,21 +226,21 @@ def termux_notification():
 
                 start_min = next_notification_min
 
-                print(f"{yellow}{bold}\n------ Getting Random Notification ------{reset}")
+                print(f"{YELLOW}{BOLD}\n------ Getting Random Notification ------{RESET}")
 
                 if start_min == 0:
-                    print(f"{dim}Next Notification at : --- {current_hour - 1}:0{start_min + notificationDelay} ---{reset}")
+                    print(f"{DIM}Next Notification at : --- {current_hour - 1}:0{start_min + notificationDelay} ---{RESET}")
 
                 elif start_min + notificationDelay < 10:
-                    print(f"{dim}Next Notification at : --- {current_hour}:0{start_min + notificationDelay} ---{reset}")
+                    print(f"{DIM}Next Notification at : --- {current_hour}:0{start_min + notificationDelay} ---{RESET}")
 
                 elif start_min + notificationDelay == 60:
-                    print(f"{dim}Next Notification at : --- {current_hour + 1}:00 ---{reset}")
+                    print(f"{DIM}Next Notification at : --- {current_hour + 1}:00 ---{RESET}")
 
                 else:
-                    print(f"{dim}Next Notification at : --- {current_hour}:{start_min + notificationDelay} ---{reset}")
+                    print(f"{DIM}Next Notification at : --- {current_hour}:{start_min + notificationDelay} ---{RESET}")
 
-                print(f"{dim}Press \' ctrl + c \' to Cancel...{reset}\n")
+                print(f"{DIM}Press \' ctrl + c \' to Cancel...{RESET}\n")
                 sleep(notificationDelay * 60 - 3)
 
         except KeyboardInterrupt:
@@ -236,9 +251,11 @@ def termux_notification():
             print("\n")
             exit()
 
+## --------------------------------------------------------------------------
+# Function to adjust volumw od audio streams
 def volume_change(vol_name):
     while True:
-        print(f"{yellow}{bold}\n------ {vol_name} ------\n{reset}")
+        print(f"{YELLOW}{BOLD}\n------ {vol_name} ------\n{RESET}")
         try:
             volume = input(f"Enter {vol_name} Volume [0 - 100] : ")
 
@@ -252,20 +269,22 @@ def volume_change(vol_name):
                 break
 
             else:
-                print(f"{red}\nInvalid Volume input. Please enter Integer value between "
-                        f"[0 - 100]\n{dim}Just press \'Enter\' to Exit.{reset}")
+                print(f"{RED}\nInvalid Volume input. Please enter Integer value between "
+                        f"[0 - 100]\n{DIM}Just press \'Enter\' to Exit.{RESET}")
 
         except:
-            print(f"{red}\nInvalid Volume input. Please enter Integer value between [0 - 100]\n"
-                    f"{dim}Just press \'Enter\' to Exit.{reset}")
+            print(f"{RED}\nInvalid Volume input. Please enter Integer value between [0 - 100]\n"
+                    f"{DIM}Just press \'Enter\' to Exit.{RESET}")
 
+###
+# volume control
 def termux_volume():
 
     while True :
-        print(f"{yellow}{bold}\n------ Volume Adjust ------\n{reset}")
+        print(f"{YELLOW}{BOLD}\n------ Volume Adjust ------\n{RESET}")
 
-        volume_input = input(f"{cyan}0. Exit       1. System        2. Call        3. Ring\n"
-                                f"4. Music      5. Notification       6. Alarm{reset}\n\n---> ")
+        volume_input = input(f"{CYAN}0. Exit       1. System        2. Call        3. Ring\n"
+                                f"4. Music      5. Notification       6. Alarm{RESET}\n\n---> ")
 
         if volume_input in ['',' ','0'] :
             print()
@@ -283,14 +302,43 @@ def termux_volume():
         elif volume_input == '6' :
             volume_change("Alarm")
         else :
-            print(f"{red}{bold}\nUnrecognised Input{reset}\n{red}{dim}Select any from [1 - 6]{reset}\n")
+            print(f"{RED}{BOLD}\nUnrecognised Input{RESET}\n{RED}{DIM}Select any from [1 - 6]{RESET}\n")
+
+## --------------------------------------------------------------------------
+# Function to set all audio streams volume to 0
+def silent_phone():
+    os.system(f'termux-volume system 0')
+    os.system(f'termux-volume call 0')
+    os.system(f'termux-volume ring 0')
+    os.system(f'termux-volume music 0')
+    os.system(f'termux-volume notification 0')
+    os.system(f'termux-volume alarm 0')
+
+    print(f"{CYAN}\nAll Audio Streams are set to \'0\'{RESET}\n\n")
+
+## --------------------------------------------------------------------------
+# Function to set all audio streams volume to 100
+def volume_up():
+    os.system(f'termux-volume system 100')
+    os.system(f'termux-volume call 100')
+    os.system(f'termux-volume ring 100')
+    os.system(f'termux-volume music 100')
+    os.system(f'termux-volume notification 100')
+    os.system(f'termux-volume alarm 100')
+
+    print(f"{CYAN}\nAll Audio Streams are set to \'100\'{RESET}\n\n")
+
+
+### ===========================================================================
+## Main function to execute all the steps
+#
 
 def mySettings():
-    print(f"{yellow}{bold}Choose the following function to perform on your phone :-\n{reset}")
+    print(f"{YELLOW}{BOLD}Choose the following function to perform on your phone :-\n{RESET}")
 
-    setting = input(f"{light_cyan}0. Exit         1. Torch on/off         "
+    setting = input(f"{LIGHT_CYAN}0. Exit         1. Torch on/off         "
                     f"2. Call \n3. Send SMS     4. WIFI on/off     5. Get Random Notifications\n"
-                    f"6. Volume adjust\n\n---> {reset}")
+                    f"6. Volume adjust     7. Silent Phone     8. Volume Up\n\n---> {RESET}")
 
     if setting in ['','0','exit'] :
         os.system("clear")
@@ -314,11 +362,20 @@ def mySettings():
     elif setting == '6' :
         termux_volume()
 
+    elif setting == '7' :
+        silent_phone()
+
+    elif setting == '8' :
+        volume_up()
+
     else:
-        print(f"{red}{bold}\nUnrecognised Input{reset}\n{red}{dim}Select any from [0 - 5]{reset}\n")
+        print(f"{RED}{BOLD}\nUnrecognised Input{RESET}\n{RED}{DIM}Select any from [0 - 5]{RESET}\n")
 
 
-while True:
-    os.system("clear")
-    mySettings()
-    input(f"{dim}Press \'Enter\' to continue.... {reset} ")
+### MAIN
+
+if __name__ == '__main__':
+    while True:
+        os.system("clear")
+        mySettings()
+        input(f"{DIM}Press \'Enter\' to continue.... {RESET} ")
